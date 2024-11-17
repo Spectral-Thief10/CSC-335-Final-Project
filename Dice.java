28897631fceb61e03512123a11ec552d7e55174e
ewzheng <ezheng@arizona.edu>
2024-11-17 15:22:02 -0700

    Added equals method to Dice, updated testing for DiceSet

--- Dice.java
+++ Dice.java
@@ -21,4 +21,21 @@ public final class Dice implements Comparable<Dice> {
     public int compareTo(Dice other){
         return Integer.compare(this.VALUE, other.VALUE);
     }
+
+    @Override
+    public boolean equals(Object obj){
+        // make sure we can actually compare these
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        // cast obj as a book and compare
+        final Dice other = (Dice)obj;
+
+        return other.VALUE == this.VALUE;
+    }
 }
--- testDiceSet.java
+++ testDiceSet.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class testDiceSet {
     DiceSet d = new DiceSet();
     
@@ -9,5 +11,24 @@ public class testDiceSet {
         assertTrue(d.getResult().size() == 0);
         d.rollAll();
         assertTrue(d.getResult().size() == 5);
+    }
+
+    @Test
+    public void testRerolls(){
+        d.rollAll();
+        ArrayList<Dice> d1 = d.getResult();
+        d.rollDiceAt(new boolean[] {true, true, false, false, false});
+        ArrayList<Dice> d2 = d.getResult();
+        d.rollDiceAt(new boolean[] {false, false, false, true, false});
+        ArrayList<Dice> d3 = d.getResult();
+
+        assertTrue(d1.get(2).equals(d2.get(2)));
+        assertTrue(d1.get(3).equals(d2.get(3)));
+        assertTrue(d1.get(4).equals(d2.get(4)));
+
+        assertTrue(d2.get(0).equals(d3.get(0)));
+        assertTrue(d2.get(1).equals(d3.get(1)));
+        assertTrue(d2.get(2).equals(d3.get(2)));
+        assertTrue(d2.get(4).equals(d3.get(4)));
     }
 }
