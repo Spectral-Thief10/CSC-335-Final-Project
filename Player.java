1461c32511bb387aadb3743bdeb4ec02bddbcad9
Spectral-Thief10 <148111599+Spectral-Thief10@users.noreply.github.com>
2024-11-19 08:50:15 -0700

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,3 +1,46 @@
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+	private ArrayList<Category> categories;
+	private boolean finished;
+	public Player() {
+		categories = new ArrayList<>();
+		finished = false;
+		for(Category i :Category.values()) {
+			categories.add(i);
+		}
+	}
+	public int categoriesLeft() {
+		return categories.size();
+	}
+	/*
+	 * @pre categories.getIndexOf(remove) != -1
+	 */
+	public boolean removeCategory(Category remove) {
+		int index = getIndexOf(remove);
+		if(index != -1) {
+			categories.remove(index);
+			return true;
+		}
+		return false;
+	}
+	public int getIndexOf(Category cat) {
+		for(int i = 0; i < categories.size();i++) {
+			if(categories.get(i)==cat) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public boolean isDone() {
+		if(categories.size()==0) {
+			finished = true;
+		}
+		else {
+			finished = false;
+		}
+		return finished;
+	}
+	
 }
