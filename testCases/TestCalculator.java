0e7b1017fcfe9ea1bef90bfd6f290f95b1e831fa
Spectral-Thief10 <148111599+Spectral-Thief10@users.noreply.github.com>
2024-12-04 18:54:47 -0700

    Add files via upload

---
+++ testCases/TestCalculator.java
@@ -0,0 +1,126 @@
+package testCases;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.Calculator;
+import model.Dice;
+
+import java.util.ArrayList;
+
+public class TestCalculator {
+	@Test
+	public void testUpperSectionCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 1, 3, 5, 4, 3 });
+
+		assertEquals(1, Calculator.upperCalculator(dice, 1));
+		assertEquals(6, Calculator.upperCalculator(dice, 3));
+		assertEquals(4, Calculator.upperCalculator(dice, 4));
+		assertEquals(5, Calculator.upperCalculator(dice, 5));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 1, 1, 1, 3, 3 });
+
+		assertEquals(3, Calculator.upperCalculator(dice2, 1));
+		assertEquals(6, Calculator.upperCalculator(dice2, 3));
+		assertEquals(0, Calculator.upperCalculator(dice2, 4));
+		assertEquals(0, Calculator.upperCalculator(dice2, 5));
+	}
+
+	@Test
+	public void testThreeOfAKindCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 5, 5, 5, 3, 2 });
+		assertEquals(20, Calculator.threeOfAKindCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 4, 4, 4, 6 });
+		assertEquals(22, Calculator.threeOfAKindCalculator(dice2));
+
+		ArrayList<Dice> dice3 = TestHelper.createResult(new int[] { 1, 2, 3, 4, 5 });
+		assertEquals(0, Calculator.threeOfAKindCalculator(dice3));
+	}
+
+	@Test
+	public void testFourOfAKindCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 4, 2, 4, 4, 6 });
+		assertEquals(0, Calculator.fourOfAKindCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 4, 4, 4, 6 });
+		assertEquals(22, Calculator.fourOfAKindCalculator(dice2));
+
+		ArrayList<Dice> dice3 = TestHelper.createResult(new int[] { 4, 4, 4, 4, 4 });
+		assertEquals(20, Calculator.fourOfAKindCalculator(dice3));
+	}
+
+	@Test
+	public void testFullHouseCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 4, 2, 4, 4, 2 });
+		assertEquals(25, Calculator.fullHouseCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 4, 4, 1, 6 });
+		assertEquals(0, Calculator.fullHouseCalculator(dice2));
+
+		ArrayList<Dice> dice3 = TestHelper.createResult(new int[] { 2, 1, 3, 2, 1 });
+		assertEquals(0, Calculator.fullHouseCalculator(dice3));
+	}
+
+	@Test
+	public void testSmallStraightCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 4, 2, 3, 1, 5 });
+		assertEquals(30, Calculator.smallStraightCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 2, 3, 1, 6 });
+		assertEquals(30, Calculator.smallStraightCalculator(dice2));
+
+		ArrayList<Dice> dice3 = TestHelper.createResult(new int[] { 2, 4, 3, 5, 4 });
+		assertEquals(30, Calculator.smallStraightCalculator(dice3));
+
+		ArrayList<Dice> dice4 = TestHelper.createResult(new int[] { 4, 6, 3, 1, 5 });
+		assertEquals(30, Calculator.smallStraightCalculator(dice4));
+
+		ArrayList<Dice> dice5 = TestHelper.createResult(new int[] { 3, 2, 3, 1, 6 });
+		assertEquals(0, Calculator.smallStraightCalculator(dice5));
+
+		ArrayList<Dice> dice6 = TestHelper.createResult(new int[] { 2, 4, 3, 5, 6 });
+		assertEquals(30, Calculator.smallStraightCalculator(dice6));
+
+		ArrayList<Dice> dice7 = TestHelper.createResult(new int[] { 3, 4, 3, 1, 6 });
+		assertEquals(0, Calculator.smallStraightCalculator(dice7));
+
+		ArrayList<Dice> dice8 = TestHelper.createResult(new int[] { 3, 5, 3, 2, 6 });
+		assertEquals(0, Calculator.smallStraightCalculator(dice8));
+
+	}
+
+	@Test
+	public void testLargeStraightCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 4, 2, 3, 1, 5 });
+		assertEquals(40, Calculator.largeStraightCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 2, 3, 5, 6 });
+		assertEquals(40, Calculator.largeStraightCalculator(dice2));
+
+		ArrayList<Dice> dice3 = TestHelper.createResult(new int[] { 3, 2, 3, 1, 6 });
+		assertEquals(0, Calculator.largeStraightCalculator(dice3));
+
+	}
+
+	@Test
+	public void testYahtzeeCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 4, 4, 4, 4, 4 });
+		assertEquals(50, Calculator.yahtzeeCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 4, 4, 4, 5 });
+		assertEquals(0, Calculator.yahtzeeCalculator(dice2));
+
+	}
+
+	@Test
+	public void testChanceCalculator() {
+		ArrayList<Dice> dice = TestHelper.createResult(new int[] { 4, 4, 4, 4, 4 });
+		assertEquals(20, Calculator.chanceCalculator(dice));
+
+		ArrayList<Dice> dice2 = TestHelper.createResult(new int[] { 4, 2, 3, 5, 6 });
+		assertEquals(20, Calculator.chanceCalculator(dice2));
+
+	}
+
+}
---
+++ testCases/testHardMode.java
@@ -0,0 +1,189 @@
+package testCases;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import model.CPU;
+import model.Dice;
+import model.HardMode;
+import model.Mode;
+import model.ScoreSheet.Category;
+
+public class testHardMode {
+	@Test
+	public void testConstructor() {
+		ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {6,6,6,6,6});
+		CPU cpu = new CPU(Mode.HARD,0,diceSet);
+		HardMode hard = new HardMode(cpu,diceSet);
+	}
+	
+	@Test
+	public void testUpperKindRerolls() {
+		ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {3,2,3,6,6});
+		CPU cpu = new CPU(Mode.HARD,0,diceSet);
+		HardMode hard = new HardMode(cpu,diceSet);
+		boolean[] rerolls = {false,false,false,false,false};
+		rerolls = hard.upperKindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {true,true,true,false,false},rerolls);
+		cpu.removeCategory(Category.SIX);
+		
+		diceSet = TestHelper.createResult(new int[] {1,2,6,5,5});
+		cpu.setDice(diceSet);
+		rerolls = hard.upperKindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {true,true,true,false,false},rerolls);
+		cpu.removeCategory(Category.FIVE);
+		
+		diceSet = TestHelper.createResult(new int[] {1,2,6,4,4});
+		cpu.setDice(diceSet);
+		rerolls = hard.upperKindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {true,true,true,false,false},rerolls);
+		cpu.removeCategory(Category.FOUR);
+		
+		diceSet = TestHelper.createResult(new int[] {1,2,6,3,3});
+		cpu.setDice(diceSet);
+		rerolls = hard.upperKindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {true,true,true,false,false},rerolls);
+		cpu.removeCategory(Category.THREE);
+		
+		diceSet = TestHelper.createResult(new int[] {1,2,6,2,5});
+		cpu.setDice(diceSet);
+		rerolls = hard.upperKindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {true,false,true,false,true},rerolls);
+		cpu.removeCategory(Category.TWO);
+		
+		diceSet = TestHelper.createResult(new int[] {1,2,6,1,5});
+		cpu.setDice(diceSet);
+		rerolls = hard.upperKindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {false,true,true,false,true},rerolls);
+		cpu.removeCategory(Category.ONE);
+	}
+	
+	@Test public void testKindRerolls() {
+		ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {3,3,3,1,6});
+		CPU cpu = new CPU(Mode.HARD,0,diceSet);
+		HardMode hard = new HardMode(cpu,diceSet);
+		boolean[] rerolls = {false,false,false,false,false};
+		rerolls = hard.kindRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {false,false,false,true,true},rerolls);
+	}
+	
+	@Test
+	public void testFullHouseRerolls() {
+		ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {3,3,3,6,6});
+		CPU cpu = new CPU(Mode.HARD,0,diceSet);
+		HardMode hard = new HardMode(cpu,diceSet);
+		boolean[] rerolls = {false,false,false,false,false};
+		rerolls = hard.fullHouseRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {false,false,false,false,false},rerolls);
+		
+		diceSet = TestHelper.createResult(new int[] {3,3,3,6,3});
+		cpu = new CPU(Mode.HARD,0,diceSet);
+		hard = new HardMode(cpu,diceSet);
+		rerolls = new boolean[]{false,false,false,false,false};
+		rerolls = hard.fullHouseRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {true,false,false,false,false},rerolls);
+	}
+	
+	@Test
+	public void testYahtzeeRerolls() {
+			ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {3,3,3,3,4});
+			CPU cpu = new CPU(Mode.HARD,0,diceSet);
+			HardMode hard = new HardMode(cpu,diceSet);
+			boolean[] rerolls = {false,false,false,false,false};
+			rerolls = hard.yahtzeeRerolls(diceSet, new boolean[] {false,false,false,false,false});
+			assertArrayEquals(new boolean[] {false,false,false,false,true},rerolls);
+			
+			
+			diceSet = TestHelper.createResult(new int[] {1,2,3,4,5});
+			cpu = new CPU(Mode.HARD,0,diceSet);
+			hard = new HardMode(cpu,diceSet);
+			rerolls = hard.yahtzeeRerolls(diceSet, new boolean[] {false,false,false,false,false});
+			assertArrayEquals(new boolean[] {true,true,true,true,false},rerolls);
+	}
+	
+	@Test
+	public void testChanceRerolls() {
+		ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {6,6,6,5,6});
+		CPU cpu = new CPU(Mode.HARD,0,diceSet);
+		HardMode hard = new HardMode(cpu,diceSet);
+		boolean[] rerolls = {false,false,false,false,false};
+		rerolls = hard.chanceRerolls(diceSet, new boolean[] {false,false,false,false,false});
+		assertArrayEquals(new boolean[] {false,false,false,true,false},rerolls);
+	}
+
+	@Test
+	public void testChooseCategory() {
+		ArrayList<Dice> diceSet = TestHelper.createResult(new int[] {6,6,6,6,6});
+		CPU cpu = new CPU(Mode.HARD,0,diceSet);
+		HardMode hard = new HardMode(cpu,diceSet);
+		assertTrue(hard.chooseCategory(diceSet)==Category.YAHTZEE);
+		cpu.removeCategory(Category.ONE);
+		cpu.removeCategory(Category.TWO);
+		cpu.removeCategory(Category.THREE);
+		cpu.removeCategory(Category.FOUR);
+		cpu.removeCategory(Category.FIVE);
+		cpu.removeCategory(Category.SIX);
+		cpu.removeCategory(Category.FOUR_OF_A_KIND);
+		cpu.removeCategory(Category.THREE_OF_A_KIND);
+		cpu.removeCategory(Category.LARGE_STRAIGHT);
+		cpu.removeCategory(Category.SMALL_STRAIGHT);
+		cpu.removeCategory(Category.FULL_HOUSE);
+		diceSet = TestHelper.createResult(new int[] {1,4,5,2,1});
+		assertTrue(hard.chooseCategory(diceSet)==Category.CHANCE);
+		
+		cpu.removeCategory(Category.YAHTZEE);
+		assertTrue(hard.chooseCategory(diceSet)==Category.CHANCE);
+		
+		cpu = new CPU(Mode.HARD,0,diceSet);
+		hard = new HardMode(cpu,diceSet);
+		cpu.removeCategory(Category.ONE);
+		cpu.removeCategory(Category.TWO);
+		cpu.removeCategory(Category.THREE);
+		cpu.removeCategory(Category.FOUR);
+		cpu.removeCategory(Category.FIVE);
+		cpu.removeCategory(Category.SIX);
+		cpu.removeCategory(Category.FOUR_OF_A_KIND);
+		cpu.removeCategory(Category.THREE_OF_A_KIND);
+		cpu.removeCategory(Category.LARGE_STRAIGHT);
+		cpu.removeCategory(Category.SMALL_STRAIGHT);
+		cpu.removeCategory(Category.FULL_HOUSE);
+		cpu.removeCategory(Category.CHANCE);
+		assertTrue(hard.chooseCategory(diceSet)==Category.YAHTZEE);
+		
+		diceSet = TestHelper.createResult(new int[] {1,4,5,3,2});
+		cpu = new CPU(Mode.HARD,0,diceSet);
+		hard = new HardMode(cpu,diceSet);
+		assertTrue(hard.chooseCategory(diceSet)==Category.LARGE_STRAIGHT);
+		cpu.removeCategory(Category.LARGE_STRAIGHT);
+		assertTrue(hard.chooseCategory(diceSet)==Category.SMALL_STRAIGHT);
+		
+		diceSet = TestHelper.createResult(new int[] {4,4,5,4,4});
+		assertTrue(hard.chooseCategory(diceSet)==Category.FOUR_OF_A_KIND);
+		
+		diceSet = TestHelper.createResult(new int[] {5,4,5,4,4});
+		assertTrue(hard.chooseCategory(diceSet)==Category.FULL_HOUSE);
+		
+		diceSet = TestHelper.createResult(new int[] {4,1,5,4,4});
+		assertTrue(hard.chooseCategory(diceSet)==Category.THREE_OF_A_KIND);
+		
+		diceSet = TestHelper.createResult(new int[] {1,2,5,6,5});
+		assertTrue(hard.chooseCategory(diceSet)==Category.FIVE);
+		
+		cpu.removeCategory(Category.ONE);
+		diceSet = TestHelper.createResult(new int[] {1,2,1,6,5});
+		assertTrue(hard.chooseCategory(diceSet)==Category.CHANCE);
+		
+		cpu.removeCategory(Category.CHANCE);
+		diceSet = TestHelper.createResult(new int[] {1,2,1,6,5});
+		assertTrue(hard.chooseCategory(diceSet)==Category.TWO);
+		
+		for(Category category : cpu.categoriesLeft()) {
+			cpu.removeCategory(category);
+		}
+		assertTrue(hard.chooseCategory(diceSet)==Category.YAHTZEE);
+	}
+
+}
